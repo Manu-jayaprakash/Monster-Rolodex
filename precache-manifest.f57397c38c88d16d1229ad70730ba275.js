63405fbad31fa497cb16a184c86babcfb953459c
Manu-jayaprakash <32713438+Manu-jayaprakash@users.noreply.github.com>
2020-04-05 13:11:22 +0530

    Updates

---
+++ precache-manifest.f57397c38c88d16d1229ad70730ba275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dda873a2a9573ad921b18893030e0e5",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "f19c8b09a36f2fc1590b",
+    "url": "/Monster-Rolodex/static/css/main.f7c98a43.chunk.css"
+  },
+  {
+    "revision": "1e8d71802ca5d0371cbc",
+    "url": "/Monster-Rolodex/static/js/2.eaad7329.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monster-Rolodex/static/js/2.eaad7329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f19c8b09a36f2fc1590b",
+    "url": "/Monster-Rolodex/static/js/main.46019293.chunk.js"
+  },
+  {
+    "revision": "a658e400339a891b921c",
+    "url": "/Monster-Rolodex/static/js/runtime-main.018fe6c0.js"
+  }
+]);
